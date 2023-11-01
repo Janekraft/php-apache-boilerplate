64e65d495c484dff9122baabfa786f7b35237882
David Rajnoha <drajnoha@redhat.com>
2023-11-01 19:12:32 +0100

    Changed the template to plain php with Apache and MariaDB

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+# Start MariaDB
 service mariadb start
-echo "CREATE DATABASE IF NOT EXISTS nette" | mysql
-cd /app && composer install
-mysql nette < /app/database.sql
-php -S 0.0.0.0:80 -t /app/www
+
+# Create the database
+echo "CREATE DATABASE IF NOT EXISTS db" | mysql
+
+# Import the database
+mysql db < /app/database.sql
+
+# Start Apache in the foreground
+apache2ctl -D FOREGROUND
